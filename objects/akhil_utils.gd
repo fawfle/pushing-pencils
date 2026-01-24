3539b18f150cca115e7733ddfec38f91d5eb1f5e
Commandtechno <68407783+Commandtechno@users.noreply.github.com>
2026-01-24 13:55:30 -0700

    crash

--- objects/akhil_utils.gd
+++ objects/akhil_utils.gd
@@ -10,5 +10,29 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func generate_sentence() -> string:
-	return ""
+var word_list: PackedStringArray
+
+func load_wordlist() -> void:
+	var file_path = "res://wordlist.txt"
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		var content = file.get_as_text()
+		# Splits by newline and removes empty entries
+		word_list = content.split("\n", false)
+		print("Loaded %d words." % word_list.size())
+
+
+func generate_sentence(word_count: int) -> String:
+	if word_list.is_empty():
+		load_wordlist()
+	
+	var sentence = ""
+	for i in range(word_count):
+		if i!= 0:
+			sentence += " "
+		sentence += word_list[randi_range(0, word_list.size() - 1)]
+	
+	return sentence
+	
+func generate_doc_id() -> String:
+	return NanoIDGenerator.new().generate(8)
