c543b5a0274d5a08dca42b26ef194fd21be02573
Sackboy495 <sackmike2007@icloud.com>
2026-01-24 17:23:00 -0700

    book works

---
+++ objects/book.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+
+func _on_button_pressed() -> void:
+	$Pages/Page0003.visible = true
+	$Pages/Page0004.visible = true
+	$Pages/Page0001.visible = false
+	$Pages/Page0002.visible = false
+
+
+func _on_button_2_pressed() -> void:
+	$Pages/Page0001.visible = true
+	$Pages/Page0002.visible = true
+	$Pages/Page0003.visible = false
+	$Pages/Page0004.visible = false
