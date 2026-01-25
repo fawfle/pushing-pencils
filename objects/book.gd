317e8d544103bfd221668780816796aaa8d0ebe1
Sackboy495 <sackmike2007@icloud.com>
2026-01-24 17:44:19 -0700

    book turn

--- objects/book.gd
+++ objects/book.gd
@@ -7,6 +7,7 @@ func _on_button_pressed() -> void:
 	$Pages/Page0004.visible = true
 	$Pages/Page0001.visible = false
 	$Pages/Page0002.visible = false
+	$AudioStreamPlayer2D.play()
 
 
 func _on_button_2_pressed() -> void:
@@ -14,3 +15,4 @@ func _on_button_2_pressed() -> void:
 	$Pages/Page0002.visible = true
 	$Pages/Page0003.visible = false
 	$Pages/Page0004.visible = false
+	$AudioStreamPlayer2D.play()
